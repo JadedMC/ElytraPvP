ef1ed18672f5a8e0cba50db9e17e0676dcdbf283
colby <colbykrawczyk10@gmail.com>
2022-09-29 22:43:51 -0400

    Added the foundation of commands

---
+++ src/main/java/net/jadedmc/elytrapvp/commands/AbstractCommand.java
@@ -0,0 +1,63 @@
+package net.jadedmc.elytrapvp.commands;
+
+import net.jadedmc.elytrapvp.ElytraPvP;
+import net.jadedmc.elytrapvp.utils.chat.ChatUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public abstract class AbstractCommand implements CommandExecutor {
+    private final String permission;
+    private final boolean canConsoleUse;
+    private static ElytraPvP plugin;
+
+    /**
+     * Creates a new AbstractCommand.
+     * @param commandName Name of the command.
+     * @param permission Permission required to use the command.
+     * @param canConsoleUse Whether or not console can use the command.
+     */
+    public AbstractCommand(final String commandName, final String permission, final boolean canConsoleUse) {
+        this.permission = permission;
+        this.canConsoleUse = canConsoleUse;
+        plugin.getCommand(commandName).setExecutor(this);
+    }
+
+    /**
+     * Registers all commands in the plugin.
+     * @param pl Plugin.
+     */
+    public static void registerCommands(ElytraPvP pl) {
+        plugin = pl;
+    }
+
+    /**
+     * Executes the command.
+     * @param sender The Command Sender.
+     * @param args Arguments of the command.
+     */
+    public abstract void execute(CommandSender sender, String[] args);
+
+    /**
+     * Processes early execution of the plugin.
+     * @param sender Command Sender.
+     * @param cmd The Command.
+     * @param label Command Label.
+     * @param args Command Arugments.
+     * @return Successful Completion.
+     */
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(!permission.equals("") && !sender.hasPermission(permission)) {
+            ChatUtils.chat(sender, "&cError &8» &cYou do not have access to that command.");
+            return true;
+        }
+        if(!canConsoleUse && !(sender instanceof Player)) {
+            ChatUtils.chat(sender, "&cError &8» &cOnly players can use that command.");
+            return true;
+        }
+        execute(sender, args);
+        return true;
+    }
+}
