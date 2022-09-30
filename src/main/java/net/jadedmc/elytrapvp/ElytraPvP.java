2b04a4f1c9cf9673ce5763e71c68d35b7695452f
colby <colbykrawczyk10@gmail.com>
2022-09-29 22:09:53 -0400

    Added some base utilities

--- src/main/java/net/jadedmc/elytrapvp/ElytraPvP.java
+++ src/main/java/net/jadedmc/elytrapvp/ElytraPvP.java
@@ -1,5 +1,9 @@
 package net.jadedmc.elytrapvp;
 
+import net.jadedmc.elytrapvp.utils.gui.GUIListeners;
+import net.jadedmc.elytrapvp.utils.scoreboard.ScoreboardListeners;
+import net.jadedmc.elytrapvp.utils.scoreboard.ScoreboardUpdate;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ElytraPvP extends JavaPlugin {
@@ -8,6 +12,13 @@ public final class ElytraPvP extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
+
+        // Utility Listeners
+        Bukkit.getPluginManager().registerEvents(new GUIListeners(), this);
+        Bukkit.getPluginManager().registerEvents(new ScoreboardListeners(), this);
+
+        // Utility Tasks
+        new ScoreboardUpdate().runTaskTimer(this, 20L, 20L);
     }
 
     @Override
---
+++ src/main/java/net/jadedmc/elytrapvp/utils/scoreboard/CustomScoreboard.java
@@ -0,0 +1,24 @@
+package net.jadedmc.elytrapvp.utils.scoreboard;
+
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class CustomScoreboard {
+    private static HashMap<UUID, CustomScoreboard> players = new HashMap<>();
+
+    public CustomScoreboard() {
+
+    }
+
+    public static HashMap<UUID, CustomScoreboard> getPlayers() {
+        return players;
+    }
+
+    public void addPlayer(Player p) {
+        getPlayers().put(p.getUniqueId(), this);
+    }
+
+    public void update(Player p) {}
+}
